1c05a2ee19e3da0465ace3569312406bc0a7c7b5
PrakharSinghOnGit <shaansinghd1209@gmail.com>
2025-08-06 13:32:11 +0530

    removed .next directory and its contents from the repository

--- .next/static/chunks/app_page_tsx_31fcb3cc._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_ad1211fe._.js",
-  "static/chunks/_74b0d4c4._.js",
-  "static/chunks/[next]_internal_font_google_andika_64e28f16_module_e8d8311d.css"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_page_tsx_905a9570._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_d309fd7e._.js",
-  "static/chunks/_9dba3449._.js",
-  "static/chunks/[next]_internal_font_google_andika_64e28f16_module_e8d8311d.css"
-],
-    source: "dynamic"
-});
