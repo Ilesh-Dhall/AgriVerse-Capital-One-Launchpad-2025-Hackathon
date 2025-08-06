c7d137425cdb8a9ac8712ccde3b3e1e9e215057e
PrakharSinghOnGit <shaansinghd1209@gmail.com>
2025-08-06 18:24:35 +0530

    Shifting to webpage cause ilesh wants to

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,80 @@
-import WeatherCard from "@/components/WeatherCard";
-import ProfileCard from "@/components/ProfileCard";
-import CropCard from "@/components/CropCard";
-import { Andika } from "next/font/google";
-import ChatCard from "@/components/ChatCard";
-import HistoryCard from "@/components/HistoryCard";
-import Seperator from "@/components/ui/seperator";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { ArrowRight, ArrowUpRight, Leaf } from "lucide-react";
+import { Caprasimo } from "next/font/google";
 
-const a = Andika({ weight: "700", subsets: ["latin"] });
-
-const Crops = [
-  { name: "Wheat", heath: 2 },
-  { name: "Rice", heath: 1 },
-  { name: "Corn", heath: 3 },
-];
-
-const chatHistory = [
-  {
-    message: "What is the best time to plant wheat?",
-    response: "Spring is ideal.",
-  },
-  {
-    message: "How to care for rice crops?",
-    response: "Keep them well-watered.",
-  },
-  {
-    message: "What are the benefits of corn?",
-    response: "High in fiber and vitamins.",
-  },
-  {
-    message: "How to improve soil health?",
-    response: "Use organic fertilizers.",
-  },
-];
+const caprasimo = Caprasimo({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function Home() {
   return (
-    <div className={a.className + " flex flex-col p-5 gap-3"}>
-      <ProfileCard name="illesh" />
-      <WeatherCard weather="Cloudy" degree={22} />
-      <CropCard crops={Crops} />
-      <Seperator heading="Ask Question" />
-      <ChatCard />
-      <Seperator heading="History" />
-      <HistoryCard histoy={chatHistory} />
+    <div>
+      <section className="bg-background h-screen flex flex-col items-center">
+        <nav className="flex justify-between max-w-6xl w-full p-8">
+          <div className="flex gap-2 items-center justify-center">
+            <Leaf color="green" strokeWidth={3} size={30} />
+            <h1 className="font-logo text-2xl font-black">Agriverse</h1>
+          </div>
+          <ul className="flex gap-8 items-center text-gray-800">
+            <li className="hover:underline underline-offset-2 transition-all hover:font-bold hover:text-green-900">
+              <a href="/about">About Us</a>
+            </li>
+            <li className="hover:underline underline-offset-2 transition-all hover:font-bold hover:text-green-900">
+              <a href="/products">Products</a>
+            </li>
+            <li className="hover:underline underline-offset-2 transition-all hover:font-bold hover:text-green-900">
+              <a href="/demo">Demo</a>
+            </li>
+            <li className="hover:underline underline-offset-2 transition-all hover:font-bold hover:text-green-900">
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+          <div>
+            <Button
+              className="rounded-4xl px-5 border-green-800"
+              variant="outline"
+              size={"lg"}
+            >
+              Join Now
+              <ArrowUpRight strokeWidth={3} />
+            </Button>
+          </div>
+        </nav>
+        <main className="flex grow w-full justify-center items-center">
+          <div className="w-1/2 h-full relative ">
+            <div className="absolute shadow-md overflow-hidden rounded-lg -rotate-12 top-40 left-40">
+              <Image
+                src={"/template.png"}
+                alt="tem"
+                width={300}
+                height={300}
+              ></Image>
+              More like this will be added later
+            </div>
+          </div>
+          <div className="w-1/2">
+            <h1
+              className={`${caprasimo.className} font-light text-7xl text-green-900 w-0`}
+            >
+              Your Intelligent Farming Companion
+            </h1>
+            <h2 className="text-xl mt-4 mb-8 w-100">
+              Get instant, expert advice for your crops in your own language
+            </h2>
+            <Button
+              variant={"outline"}
+              size={"lg"}
+              className="p-7 rounded-full text-xl font-logo border-green-800 border-2"
+            >
+              Ask AgriVerse
+              <ArrowRight strokeWidth={3} className="" />
+            </Button>
+          </div>
+        </main>
+      </section>
+      <section className="bg-white">Rest of the page</section>
     </div>
   );
 }
